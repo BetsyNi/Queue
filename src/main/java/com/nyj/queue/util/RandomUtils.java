f6bbb90ab0179ad88c687226377774b5235e3d64
Xinling Jing <jingxinling1024@163.com>
2019-04-21 01:30:40 +0800

    1. 增加生成随机时间工具类

--- src/main/java/com/nyj/queue/util/RandomUtils.java
+++ src/main/java/com/nyj/queue/util/RandomUtils.java
@@ -5,9 +5,10 @@ import com.nyj.queue.exception.BusinessException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 /**
- * 随机数工具类
+ * 随机数工具类-注意:此类不保证线程安全
  *
  * @author: Xinling Jing
  * @date: 2019/4/20 0020 19:38
@@ -16,30 +17,69 @@ import java.util.Date;
 public class RandomUtils {
 
     private static SimpleDateFormat format = null;
+    private static Random random = null;
+    private static final int THOUSAND = 1000;
+
 
     static {
         format = new SimpleDateFormat("HH:mm:ss");
+        random = new Random();
     }
 
     /**
-     * 生成一天内的随机时间，格式：HH:mm:ss
+     * 获取一天内的随机时间戳,单位 ms
+     *
+     * @return 该随机时间戳自当天 08:00:00 之后所经过的毫秒数
      */
-    public static String randomTime() throws ParseException {
+    public static long randomTime() throws ParseException {
         return randomTime("00:00:00", "23:59:59");
     }
 
     /**
-     * 返回一天内指定时间范围内的随机时间
+     * 以易读格式获取一天指定时间范围内时间,格式: HH:mm:ss
      *
-     * @param beginTime 格式要求： HH:mm:ss
-     * @param endTime   格式要求： HH:mm:ss
+     * @return 以 HH:mm:ss 格式返回随机时间
      */
-    public static String randomTime(String beginTime, String endTime) throws ParseException {
+    public static String randomTimeForHuman() throws ParseException {
+        long randomNum = RandomUtils.randomTime();
+        Date date = new Date(randomNum);
+        return RandomUtils.format.format(date);
+    }
+
+    /**
+     * 获取一天内指定时间范围内的随机时间戳,单位 ms
+     *
+     * @param beginTime 格式要求: HH:mm:ss
+     * @param endTime   格式要求: HH:mm:ss
+     * @return 该随机时间戳自当天 08:00:00 之后所经过的毫秒数
+     */
+    public static long randomTime(String beginTime, String endTime) throws ParseException {
         Date begin = format.parse(beginTime);
         Date end = format.parse(endTime);
         if (begin.compareTo(end) >= 0) {
             throw new BusinessException(1001, "End time can not less than begin time");
         }
-        return "";
+        // [m, n]
+        int tmp = random.nextInt((int) (end.getTime() / THOUSAND - begin.getTime() / THOUSAND + 1));
+        return (begin.getTime() + tmp) * THOUSAND;
+    }
+
+    /**
+     * 以易读格式获取一天指定时间范围内时间,格式: HH:mm:ss
+     *
+     * @param beginTime 格式要求: HH:mm:ss
+     * @param endTime   格式要求: HH:mm:ss
+     * @return 以 HH:mm:ss 格式返回随机时间
+     */
+    public static String randomTimeForHuman(String beginTime, String endTime) throws ParseException {
+        long randomNum = RandomUtils.randomTime(beginTime, endTime);
+        Date date = new Date(randomNum);
+        return RandomUtils.format.format(date);
+    }
+
+    public static void main(String[] args) throws ParseException {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(RandomUtils.randomTimeForHuman("09:00:00", "12:00:00"));
+        }
     }
 }
